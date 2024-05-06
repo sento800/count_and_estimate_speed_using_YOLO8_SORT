4adaccc7ddfd797981c367cedcc9deb2d6bcdb82
phuhh <phuhhhhh5@gmail.com>
2024-05-06 10:03:00 +0700

    project

--- estimateSpeed.py
+++ estimateSpeed.py
@@ -8,6 +8,7 @@ def estimateSpeed(location1, location2):
 	ppm = 9
 	d_meters = d_pixels / ppm
 	print("d_pixels=" + str(d_pixels), "d_meters=" + str(d_meters))
-	# fps = 15
-	speed = d_meters * 3.6 # * fps
+	fps = 15
+	time_elapsed = 1 / fps * 3.6
+	speed = d_meters * time_elapsed
 	return speed
